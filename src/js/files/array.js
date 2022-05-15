bdbe92ee6b4c92c1a683313cb2f183786989d989
11VladC11 <vladw239@gmail.com>
2022-05-15 09:16:38 +0300

    preparing array option

---
+++ src/js/files/array.js
@@ -0,0 +1,68 @@
+const storage = [
+	//home
+	{
+		"name":"Name",
+		"status":"student",
+		"img":"",
+	},
+	//about
+	{
+		"aboutMe":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, voluptatem quae doloribus cumque aspernatur commodi temporibus eligendi nostrum debitis et magni dolorum molestiae quaerat laborum rem sint eius a, placeat fugit facilis. Consequuntur nemo ducimus tenetur at architecto ab voluptate incidunt molestiae blanditiis officia accusamus eum error ipsam, deleniti placeat.",
+		"skills":["reading","drawing","painting","learning","speaking","English","Romanian"],
+
+		"education":{
+			"firstEd":{
+				"date":"2013 - 2016",
+				"univType":"bachelor of technology - ",
+				"univ":"todford university",
+				"description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolorem quos ex quidem earum velit!"
+			},
+			"secondEd":{
+				"date":"2013 - 2016",
+				"univType":"bachelor of technology - ",
+				"univ":"todford university",
+				"description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolorem quos ex quidem earum velit!"
+			},
+			"thirdEd":{
+				"date":"2013 - 2016",
+				"univType":"bachelor of technology - ",
+				"univ":"todford university",
+				"description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolorem quos ex quidem earum velit!"
+			}
+		},
+		"experience":{
+			"firstExperience":{
+				"date":"2013 - 2016",
+				"function":"web developer -",
+				"place":"the webshala",
+				"description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum optio ea ex esse nobis repellat.",
+			},
+			"secondExperience":{
+				"date":"2013 - 2016",
+				"function":"web developer -",
+				"place":"the webshala",
+				"description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum optio ea ex esse nobis repellat.",
+			},
+		}
+	},
+	//portfolio
+	{
+		"img":"https://image.shutterstock.com/image-vector/certificate-diploma-template-sample-text-600w-259489280.jpg",
+		"title":"education course website",
+		"description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci natus accusamus pariatur asperiores aperiam corporis, quam nemo distinctio nihil. Quasi, necessitatibus id voluptate unde error iste quo eum minima reprehenderit, quod ab at blanditiis assumenda, vel a officiis optio? Dolorum fugiat neque praesentium, repellat et enim sit voluptate maxime delectus.",
+		"created":"4 Dec 2020",
+		"techUsed":"Html, Css",
+		"role":"Frontend",
+		"onlineLink":"www.domain.com"
+	},
+	//contact
+	{
+		"email":"example@gmail.com",
+		"phone":"91 98 **** ****",
+		"facebook":"",
+		"twitter":"",
+		"instagram":"",
+		"linkedIn":"",
+		"GitHub":"",
+	}
+]
